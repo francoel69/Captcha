a8f423ad8ca9fb20e920dac89344f2d7a6d29ba5
Franco Rodriguez <francoel69@gmail.com>
2011-07-30 15:28:53 -0300

    Modificaciones al disenho de los captchas.

--- captcha.py
+++ captcha.py
@@ -1,140 +1,7 @@
 # -*- coding: utf-8 -*-
-import Image
-import ImageFilter
 import random
 import math
-import os
-
-
-class InCaptcha():
-    """ Clase para tratar captchas. """
-
-    def __init__(self, name):
-        self.incap = Image.open(name)
-        self.nx, self.ny = self.incap.size
-        self.limites = []
-        self.letras = []
-
-    def quitarRuido(self):
-        self.incap = self.incap.filter(ImageFilter.ModeFilter())
-        colors = self.incap.getcolors()
-        colors.sort(reverse=True)
-        colors = map(lambda (x, y): y, colors[:6])
-        for x in range(self.nx):
-            for y in range(self.ny):
-                t = self.incap.getpixel((x, y))
-                if not t in colors:
-                    self.incap.putpixel((x, y), (255, 255, 255))
-        colors.remove((255, 255, 255))
-        self.dividir(colors)
-
-    def __proxcolumnacolor(self, desde, colors):
-        x, cond = desde, True
-        while (x < self.nx) and cond:
-            y = 0
-            while (y < self.ny) and cond:
-                t = self.incap.getpixel((x, y))
-                if t in colors:
-                    cond, ret = False, x
-                y += 1
-            x += 1
-#        if x == self.nx and cond:
-#            raise Warning("Imagen complicada.")
-        return (ret, t)
-
-    def __ultimacolumnacolor(self, desde, color):
-        x, cond = desde, True
-        while (x < self.nx) and cond:
-            y, cond2 = 0, True
-            while (y < self.ny) and cond2:
-                t = self.incap.getpixel((x, y))
-                if(t == color):
-                    cond2 = False
-                y += 1
-            if(y == self.ny):
-                cond, ret = False, x - 1
-            x += 1
-        if (x == self.nx):
-            ret = x - 1
-        return ret
-
-    def __primerfilacolor(self, xdesde, xhasta, color):
-        y, cond = 0, True
-        while (y < self.ny) and cond:
-            x = xdesde
-            while (x <= xhasta) and cond:
-                t = self.incap.getpixel((x, y))
-                if(t == color):
-                    cond, ret = False, y
-                x += 1
-            y += 1
-        return ret
-
-    def __ultimafilacolor(self, xdesde, xhasta, ydesde, color):
-        y, cond = ydesde, True
-        while(y < self.ny) and cond:
-            x, cond2 = xdesde, True
-            while(x <= xhasta) and cond2:
-                t = self.incap.getpixel((x, y))
-                if(t == color):
-                    cond2 = False
-                else:
-                    x += 1
-            if(cond2):
-                cond, ret = False, y - 1
-            y += 1
-        if (y == self.ny):
-            ret = y - 1
-        return ret
-
-    def dividir(self, colors):
-        left = 0
-        for i in range(5):
-            left, color = self.__proxcolumnacolor(left, colors)
-            colors.remove(color)
-            right = self.__ultimacolumnacolor(left, color)
-            upper = self.__primerfilacolor(left, right, color)
-            lower = self.__ultimafilacolor(left, right, upper, color)
-            self.limites.append((left, upper, right + 1, lower + 1))
-
-    def separar(self):
-        for i in range(5):
-            self.letras.append(Letra(self.incap, self.limites[i]))
-
-    def save(self, filename, *args, **kwds):
-        self.incap.save(filename, *args, **kwds)
-
-    def show(self):
-        self.incap.show()
-
-
-class Letra():
-    """ Clase para tratar las letras por separado. """
-
-    xmax, ymax = 16, 16
-
-    def __init__(self, im, box):
-        self.region = im.crop(box)
-        colors = self.region.getcolors()
-        colors.sort(reverse=True)
-        color = colors[0][1]
-        if color == (255, 255, 255):
-            color = colors[1][1]
-        self.color = color
-
-    def resize(self, (x, y)=(xmax, ymax)):
-        self.region = self.region.resize((x, y))
-
-    def rellenarvector(self):
-        self.matriz = []
-        for x in range(0, self.xmax):
-            for y in range(0, self.ymax):
-                t = self.region.getpixel((x, y))
-                if(t == self.color):
-                    self.matriz.append(int(1))
-                else:
-                    self.matriz.append(int(0))
-
+import imagein
 
 inputs = 256
 c_ocultas = 1
@@ -143,10 +10,149 @@ outputs = 36
 eta = 0.2
 error_t = 0.1
 
+def g(x):
+    return (1.0 / (1.0 + math.exp(-x)))
 
-def inicializar_inputs(im):
+
+def gp(x):
+    return (g(x) * (1.0 - g(x)))
+
+# Make a matrix IxJ.
+def makeMatrix(I, J, fill=0.0):
+    m = []
+    for i in range(I):
+        m.append([fill]*J)
+    return m
+
+
+class Net:
+    """ Clase contenedora de las redes neuronales. """
+
+    def __init__(self):
+        # Number of input, hidden, and output nodes.
+        self.ni = inputs
+        self.nh = n_ocultas
+        self.no = outputs
+
+        # Activations for nodes.
+        self.hi = [1.0]*self.ni
+        self.hh = [1.0]*self.nh
+        self.ho = [1.0]*self.no
+
+        # Set weights.
+        self.wi = setWeights(self.nh, self.ni, "../pesos.txt")
+        self.wo = setWeights(self.no, self.nh, "../pesos2.txt")
+
+        # Last change in weights for momentum.
+        #self.ci = makeMatrix(self.nh, self.ni)
+        #self.co = makeMatrix(self.no, self.nh)
+
+    def update(self, inputs):
+        if len(inputs) != self.ni:
+            raise ValueError('Wrong number of inputs.')
+
+        # Input activations.
+        for i in range(self.ni):
+            self.hi[i] = inputs[i]
+
+        # Hidden activations.
+        for j in range(self.nh):
+            sum = 0.0
+            for i in range(self.ni):
+                sum = sum + self.hi[i] * self.wi[j][i]
+            self.hh[j] = sum
+
+        # Output activations.
+        for k in range(self.no):
+            sum = 0.0
+            for j in range(self.nh):
+                sum = sum + g(self.hh[j]) * self.wo[k][j]
+            self.ho[k] = sum
+
+        return maxout(map(g, self.ho))
+
+    def backPropagate(self, targets, N=0.5, M=0.1):
+        if len(targets) != self.no:
+            raise ValueError('Wrong number of target values.')
+
+        # calculate error terms for output
+        output_deltas = [0.0] * self.no
+        for k in range(self.no):
+            error = targets[k]-g(self.ho[k])
+            output_deltas[k] = gp(self.ho[k]) * error
+
+        # calculate error terms for hidden
+        hidden_deltas = [0.0] * self.nh
+        for j in range(self.nh):
+            error = 0.0
+            for k in range(self.no):
+                error = error + output_deltas[k]*self.wo[k][j]
+            hidden_deltas[j] = gp(self.hh[j]) * error
+
+        # update output weights
+        for j in range(self.nh):
+            for k in range(self.no):
+                change = output_deltas[k]*g(self.hh[j])
+                self.wo[k][j] = self.wo[k][j] + N*change
+                #self.co[k][j] = change
+                #print N*change, M*self.co[j][k]
+
+        # update input weights
+        for i in range(self.ni):
+            for j in range(self.nh):
+                change = hidden_deltas[j]*self.hi[i]
+                self.wi[j][i] = self.wi[j][i] + N*change
+                #self.ci[j][i] = change
+
+        # calculate error
+        error = 0.0
+        for k in range(len(targets)):
+            error = error + 0.5*(targets[k]-g(self.ho[k]))**2
+        return error
+
+    def test(self, patterns):
+        for p in patterns:
+            print p[0], '->', self.update(p[0])
+
+    def weights(self):
+        print('Input weights:')
+        for i in range(self.ni):
+            print(self.wi[i])
+        print()
+        print('Output weights:')
+        for j in range(self.nh):
+            print(self.wo[j])
+
+    def error_ok(self, targets):
+        lista = []
+        for i in range(self.no):
+            lista.append(abs(g(self.ho[i]) - targets[i]))
+        return any(map(lambda x: x>error_t, lista))
+
+    def train(self, files, folder, iterations=10, N=0.5, M=0.1):
+        # N: learning rate
+        # M: momentum factor
+        dic = targetDic(folder)
+        for k in range(iterations):
+            error = 0.0
+            for im in files:
+                if dic.has_key(im):
+                    xi = getInputs(folder + "/" + im)
+                    for i in range(5):
+                        targets = getTarget(dic.get(im)[i])
+                    #first = True
+                    #while first or self.error_ok(targets):
+                        #first = False
+                        self.update(xi[i])
+                        error = error + self.backPropagate(targets, N, M)
+                    #error = error + err
+            print 'error', error/len(files)
+        print u"Terminó de entrenar"
+
+
+def getInputs(im):
     xi = []
-    cap = InCaptcha(im)
+    cap = imagein.InCaptcha(im)
     cap.quitarRuido()
     cap.separar()
     for i in range(5):
@@ -156,25 +162,25 @@ def inicializar_inputs(im):
     return xi
 
 
-def inicializar_pesos(filas, columnas, file):
+def setWeights(nrows, ncols, file):
     w = []
     try:
         f = open(file, "r")
     except IOError:
-        for i in range(filas):
-            fila = []
-            for j in range(columnas):
-                fila.append(random.uniform(-0.05, 0.05))
-            w.append(fila)
+        for i in range(nrows):
+            row = []
+            for j in range(ncols):
+                row.append(random.uniform(-0.1, 0.1))
+            w.append(row)
     else:
         l = f.readlines()
         f.close()
-        for i in range(filas):
+        for i in range(nrows):
             w.append(map(lambda x: float(x), l[i].split()))
     return w
 
 
-def obtener_output(l):
+def getTarget(l):
     out = ord(l)
     if (48 <= out <= 57):
         pos = out - 48
@@ -185,63 +191,14 @@ def obtener_output(l):
     return letra
 
 
-def output_correcto(folder):
+def targetDic(folder):
     hash = open(folder + "/hash.txt", "r")
     dic = dict(map(lambda l: l.split('\t'), hash.readlines()))
     hash.close()
     return dic
 
 
-def calcular_h1(w, xi, n):
-    h1 = []
-    for i in range(n_ocultas):
-        sum = 0.0
-        for j in range(inputs):
-            sum += xi[n][j] * w[i][j]
-        h1.append(sum)
-    return h1
-
-
-def calcular_h2(h1, w2):
-    h2 = []
-    for i in range(outputs):
-        sum = 0.0
-        for j in range(n_ocultas):
-            sum += g(h1[j]) * w2[i][j]
-        h2.append(sum)
-    return h2
-
-
-def computar_delta2(h2, z):
-    d2 = []
-    for i in range(outputs):
-        d2.append(gp(h2[i]) * (z[i] - g(h2[i])))
-    return d2
-
-
-def computar_delta1(h1, w2, d2):
-    d1 = []
-    for i in range(n_ocultas):
-        sum = 0.0
-        for j in range(outputs):
-            sum += w2[j][i] * d2[j]
-        d1.append(gp(h1[i]) * sum)
-    return d1
-
-
-def actualizar_w1(w1, d1, xi, n):
-    for i in range(n_ocultas):
-        for j in range(inputs):
-            w1[i][j] += eta * d1[i] * xi[n][j]
-
-
-def actualizar_w2(w2, d2, h1):
-    for i in range(outputs):
-        for j in range(n_ocultas):
-            w2[i][j] += eta * d2[i] * g(h1[j])
-
-
-def actualizar_archivo(w, file):
+def updateFile(w, file):
     filas = len(w)
     f = open(file, "w")
     for i in range(filas):
@@ -250,22 +207,7 @@ def actualizar_archivo(w, file):
     f.close()
 
 
-def g(x):
-    return (1.0 / (1.0 + math.exp(-x)))
-
-
-def gp(x):
-    return (g(x) * (1.0 - g(x)))
-
-
-def error_ok(h2, z):
-    lista = []
-    for i in range(outputs):
-        lista.append(abs(g(h2[i]) - z[i]))
-    return any(map(lambda x: x>error_t, lista))
-
-
-def representante(gh2):
+def maxout(gh2):
     pmax = gh2.index(max(gh2))
     if 0<= pmax <= 9:
         ret = repr(pmax)
@@ -274,64 +216,29 @@ def representante(gh2):
     return ret
 
 
-def process(im):
-    w = inicializar_pesos(n_ocultas, inputs, "../pesos.txt")
-    w2 = inicializar_pesos(outputs, n_ocultas, "../pesos2.txt")
-    ret = []
-    xi = inicializar_inputs(im)
-    for i in range(5):
-        first = False
-        h1 = calcular_h1(w, xi, i)
-        h2 = calcular_h2(h1, w2)
-        ret.append(representante(map(g, h2)))
-    return ret
+#def process(im):
+    #w = inicializar_pesos(n_ocultas, inputs, "../pesos.txt")
+    #w2 = inicializar_pesos(outputs, n_ocultas, "../pesos2.txt")
+    #ret = []
+    #xi = inicializar_inputs(im)
+    #for i in range(5):
+        #h1 = calcular_h1(w, xi, i)
+        #h2 = calcular_h2(h1, w2)
+        #ret.append(representante(map(g, h2)))
+    #return ret
 
 
-def modify(im, out):
-    w = inicializar_pesos(n_ocultas, inputs, "../pesos.txt")
-    w2 = inicializar_pesos(outputs, n_ocultas, "../pesos2.txt")
-    xi = inicializar_inputs(im)
-    for i in range(5):
-        zi = obtener_output(out[i])
-        h1 = calcular_h1(w, xi, i)
-        h2 = calcular_h2(h1, w2)
-        d2 = computar_delta2(h2, zi)
-        d1 = computar_delta1(h1, w2, d2)
-        actualizar_w1(w, d1, xi, i)
-        actualizar_w2(w2, d2, h1)
-    actualizar_archivo(w, "../pesos.txt")
-    actualizar_archivo(w2, "../pesos2.txt")
-
-
-#def train(folder):
-#    w = inicializar_pesos(n_ocultas, inputs, "./pesos.txt")
-#    w2 = inicializar_pesos(outputs, n_ocultas, "./pesos2.txt")
-#
-#    files = os.walk(folder).next()[2]
-#    if '.directory' in files:
-#        files.remove('.directory')
-#    if 'hash.txt' in files:
-#        files.remove('hash.txt')
-#    count = 0
-#    for im in files:
-#        count += 1
-#        try:
-#            xi = inicializar_inputs(folder + im)
-#            z = output_correcto(folder)
-#        except (Warning, AttributeError) as detail:
-#            print im, count, detail
-#        else:
-#            print im, count
-#            for i in range(5):
-#                first = True
-#                while first or error_ok(h2, z[i]):
-#                    first = False
-#                    h1 = calcular_h1(w, xi, i)
-#                    h2 = calcular_h2(h1, w2)
-#                    d2 = computar_delta2(h2, z[i])
-#                    d1 = computar_delta1(h1, w2, d2)
-#                    actualizar_w1(w, d1, xi, i)
-#                    actualizar_w2(w2, d2, h1)
-#    actualizar_archivo(w, "./pesos.txt")
-#    actualizar_archivo(w2, "./pesos2.txt")
-
+#def modify(im, out):
+    #w = inicializar_pesos(n_ocultas, inputs, "../pesos.txt")
+    #w2 = inicializar_pesos(outputs, n_ocultas, "../pesos2.txt")
+    #xi = inicializar_inputs(im)
+    #for i in range(5):
+        #zi = obtener_output(out[i])
+        #h1 = calcular_h1(w, xi, i)
+        #h2 = calcular_h2(h1, w2)
+        #d2 = computar_delta2(h2, zi)
+        #d1 = computar_delta1(h1, w2, d2)
+        #actualizar_w1(w, d1, xi, i)
+        #actualizar_w2(w2, d2, h1)
+    #actualizar_archivo(w, "../pesos.txt")
+    #actualizar_archivo(w2, "../pesos2.txt")
--- interfaz/interfaz.py
+++ interfaz/interfaz.py
@@ -2,7 +2,7 @@
 import sys
 import os
 from PyQt4 import QtCore, QtGui
-
+from os.path import isfile
 from untitled import Ui_MainWindow
 import captcha
 
@@ -11,10 +11,11 @@ class MyForm(QtGui.QMainWindow):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.nn = captcha.Net()
         QtCore.QObject.connect(self.ui.toolButtonBuscarCarpeta, QtCore.SIGNAL("clicked()"), self.folder_dialog)
         QtCore.QObject.connect(self.ui.toolButtonEntrenar,QtCore.SIGNAL("clicked()"), self.train)
         QtCore.QObject.connect(self.ui.toolButtonBuscarArchivo,QtCore.SIGNAL("clicked()"), self.file_dialog)
-        QtCore.QObject.connect(self.ui.toolButtonCargar, QtCore.SIGNAL("clicked()"), self.captcha_in)
+        QtCore.QObject.connect(self.ui.toolButtonCargar, QtCore.SIGNAL("clicked()"), self.show_captcha)
         QtCore.QObject.connect(self.ui.toolButtonDescifrar, QtCore.SIGNAL("clicked()"), self.process)
         QtCore.QObject.connect(self.ui.toolButtonModificar, QtCore.SIGNAL("clicked()"), self.modify)
         QtCore.QObject.connect(self.ui.pushButtonSalir, QtCore.SIGNAL("clicked()"), self.salir)
@@ -26,37 +27,42 @@ class MyForm(QtGui.QMainWindow):
 
     def train(self):
         folder = str(self.ui.lineEditCarpeta.text())
-        dic = captcha.output_correcto(folder)
+        #dic = captcha.targetDic(folder)
         files = os.walk(folder).next()[2]
         files = filter(lambda e: e.endswith(('.bmp', '.jpeg', '.jpg', '.png')), files)
-        self.ui.progressBar.setRange(0, len(dic))
-        count = 0
-        for im in files:
-            if dic.has_key(im):
-                self.ui.labelProgress.setText(im)
-                captcha.modify(folder + "/" + im, dic.get(im))
-                count += 1
-                self.ui.progressBar.setValue(count)
+        #self.ui.progressBar.setRange(0, len(dic))
+        #count = 0
+        #for im in files:
+            #if dic.has_key(im):
+                #self.ui.labelProgress.setText(im)
+                #xi = captcha.getInputs(im)
+                #for i in range(5):
+        hash = folder + "/hash.txt"
+        if(isfile(hash)):
+            self.nn.train(files, folder)
+        else:
+            print "No existe el archivo \"hash.txt\""
+                #count += 1
+                #self.ui.progressBar.setValue(count)
 
     def file_dialog(self):
         fd = QtGui.QFileDialog(self)
         self.filename = fd.getOpenFileName(caption= "Seleccionar archivo", filter="Images (*.png *.bmp *.jpg)")
-        from os.path import isfile
         if isfile(self.filename):
             self.ui.lineEditArchivo.setText(self.filename)
 
-    def captcha_in(self):
+    def show_captcha(self):
         scene = QtGui.QGraphicsScene()
         scene.addPixmap(QtGui.QPixmap(self.ui.lineEditArchivo.text()))
         self.ui.graphicsViewCaptcha.setScene(scene)
 
     def process(self):
-        res = captcha.process(str(self.ui.lineEditArchivo.text()))
-        self.ui.lineEdit.setText(res[0])
-        self.ui.lineEdit_2.setText(res[1])
-        self.ui.lineEdit_3.setText(res[2])
-        self.ui.lineEdit_4.setText(res[3])
-        self.ui.lineEdit_5.setText(res[4])
+        xi = captcha.getInputs(str(self.ui.lineEditArchivo.text()))
+        self.ui.lineEdit.setText(self.nn.update(xi[0]))
+        self.ui.lineEdit_2.setText(self.nn.update(xi[1]))
+        self.ui.lineEdit_3.setText(self.nn.update(xi[2]))
+        self.ui.lineEdit_4.setText(self.nn.update(xi[3]))
+        self.ui.lineEdit_5.setText(self.nn.update(xi[4]))
 
     def modify(self):
         l1 = str(self.ui.lineEdit.text())
@@ -64,9 +70,15 @@ class MyForm(QtGui.QMainWindow):
         l3 = str(self.ui.lineEdit_3.text())
         l4 = str(self.ui.lineEdit_4.text())
         l5 = str(self.ui.lineEdit_5.text())
-        captcha.modify(str(self.ui.lineEditArchivo.text()), [l1,l2,l3,l4,l5])
+        xi = captcha.getInputs(str(self.ui.lineEditArchivo.text()))
+        targets = map(captcha.getTarget, [l1, l2, l3, l4, l5])
+        for i in range(5):
+            self.nn.update(xi[i])
+            self.nn.backPropagate(targets[i])
 
     def salir(self):
+        captcha.updateFile(self.nn.wi, "../pesos.txt")
+        captcha.updateFile(self.nn.wo, "../pesos2.txt")
         answer = QtGui.QMessageBox.question(self.parent(), u"¿Salir?", u"¿Está seguro que desea salir?",
         QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)
         if answer == QtGui.QMessageBox.Yes:
